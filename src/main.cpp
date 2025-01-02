bad6dd04d015c72ea274170f7c6cf7f52a263e55
Lankow <maciej.lankow@gmail.com>
2025-01-02 22:32:16 +0100

    Display test.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
-#include <Arduino.h>
-#include <memory>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-void setup()
-{
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, 5, 6, -1);
+
+void setup() {
+    Serial.begin(115200);
+    
+    if (!display.begin())
+    {
+        Serial.println("SSD1306 allocation failed");
+    }
+
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.print("test");
+        display.display();
+        delay(1000);
 }
 
 void loop()
