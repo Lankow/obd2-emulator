e9238660f40590aba7f86130c7fa030cf6befedd
Lankow <maciej.lankow@gmail.com>
2025-01-03 23:28:44 +0100

    Fix for display.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -6,25 +5,26 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, 5, 6, -1);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-    Serial.begin(115200);
-    
-    if (!display.begin())
-    {
-        Serial.println("SSD1306 allocation failed");
-    }
+  Serial.begin(115200);
 
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 0);
-    display.print("test");
-        display.display();
-        delay(1000);
+  Wire.begin(14, 12); // SDA = GPIO14, SCL = GPIO12
+  Serial.println("Initializing OLED...");
+
+if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)){
+    Serial.println("Failed to initialize SSD1306!");
+    while (true); 
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("OBD2-Emulator");
+  display.display();
 }
 
-void loop()
-{
+void loop() {
 }
