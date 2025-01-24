b9528c953a502904270107709c36dd6706713943
Lankow <maciej.lankow@gmail.com>
2025-01-24 23:26:47 +0100

    Ble handler to bt handler.

--- include/BtHandler.hpp
+++ include/BtHandler.hpp
@@ -1,5 +1,5 @@
 /**
- * @file BLEHandler.hpp
+ * @file BtHandler.hpp
  * @date   2025-01-04
  * @author Lankow
  * @version 1.0
@@ -9,17 +9,19 @@
 
 #include <Arduino.h>
 #include <memory>
+#include "BluetoothSerial.h"
 #include "OBD2PIDManager.hpp"
 
-class BLEHandler
+class BtHandler
 {
     public:
-        BLEHandler(std::shared_ptr<OBD2PIDManager> manager);
+        BtHandler(std::shared_ptr<OBD2PIDManager> manager);
 
         void initialize();
         void cyclic();
         
     private:
+        BluetoothSerial m_btSerial;
         std::shared_ptr<OBD2PIDManager> m_manager;
 };  
 #endif // BlUETOOTH_HANDLER_HPP
---
+++ src/BtHandler.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file BtHandler.cpp
+ * @date   2025-01-04
+ * @author Lankow
+ * @version 1.0
+ */
+
+#include "BtHandler.hpp"
+#include "OBD2PIDInfo.hpp"
+
+BtHandler::BtHandler(std::shared_ptr<OBD2PIDManager> manager) : m_manager(manager) {}
+
+void BtHandler::initialize()
+{
+    m_btSerial.begin("OBDII-ESP");
+    Serial.println("Bluetooth started.");
+}
+
+void BtHandler::cyclic()
+{
+  if (m_btSerial.available()){
+    String message = "";
+
+    char incomingChar = m_btSerial.read();
+    if (incomingChar != '\n'){
+      message += String(incomingChar);
+    }
+    else{
+      message = "";
+    }
+  }
+}
+
